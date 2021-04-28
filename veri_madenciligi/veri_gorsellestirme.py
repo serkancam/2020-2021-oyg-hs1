122b63991a2e10ffd102da7793ba363f58167ab8
serkancam <sekocam@gmail.com>
2021-04-28 22:43:20 +0300

    Update veri_gorsellestirme.py

---
+++ veri_madenciligi/veri_gorsellestirme.py
@@ -0,0 +1,63 @@
+#%% kütüphaneler ve veri seti
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+veri=pd.read_csv("kalp_rahatsizligi.csv")
+# print(veri.head(4))
+veri.head(4)
+#%%
+veri.info()
+# %% yaş histogramı
+sns.histplot(data=veri,x="yas",bins=20)
+plt.xlabel("yaş")
+plt.ylabel("Adet")
+plt.title("Yaş hverisi histogramı")
+plt.xticks(np.arange(20,90,step=10))
+plt.show()
+
+# %% yaş histogramı talasemiye göre 
+sns.histplot(data=veri,x="yas",hue="talasemi",element="step")
+plt.xlabel("yaş")
+plt.ylabel("Adet")
+plt.title("Yaş verisi histogramı")
+plt.show()
+
+# %% yaş histogramını kalp rahatsızlığı sütununu ayırıcı olarak kullanıp grafik oluşturunuz.
+sns.histplot(data=veri,x="yas",hue="kalp_rahatsizligi",element="step",palette="BuPu")
+plt.xlabel("yaş")
+plt.ylabel("Adet")
+plt.title("Yaş verisi histogramı")
+plt.show()
+# %% saçılım grafiği:(scatterpolt)
+# Saçılım diyagramı, iki farklı değişkenin arasındaki ilişkiyi belirlemek için kullanılır. Aralarındaki ilişkinin sebebi görülemese de, ilgili iki değişkenin arasında direkt olarak bir ilişki bulunup bulunmadığı ve bu ilişkinin ne derece güçlü olduğu görülebilir.
+#%% kolestrol değeri ile en yüksek kalp hızı arasındaki saçılım grafiği
+sns.scatterplot(data=veri,x="serum_kolestrol",y="en_yuksek_kalp_hizi",hue="cinsiyet")
+plt.show()
+# %% bu veriler arasındaki ilişkiyi göreceğiz
+sns.regplot(data=veri,x="serum_kolestrol",y="en_yuksek_kalp_hizi")
+plt.show()
+# %%
+sns.regplot(data=veri,x="yas",y="en_yuksek_kalp_hizi",)
+plt.show()
+# %% çizgi grafiği sayısal verilerin ilişkisini göstermek için veya genelde zamana bağlı depğişim için çizilir.
+# yaş ve en yüksek kalp hızı ç.izgi grafiği
+
+sns.lineplot(data=veri,x="yas",y="en_yuksek_kalp_hizi",hue="kalp_rahatsizligi")
+plt.show()
+# %% sütun/çubuk grafiği: kategorik verilerin görselleştirilmesinde sıklıkla kullanılır.
+
+# cinsşyete göre kalp rahatsızlığın dağılımı
+sns.countplot(data=veri,x="cinsiyet",hue="kalp_rahatsizligi")
+plt.show()
+
+
+# %% göğüs ağrısı tipinin kalp rahatsızlığına göre dağılımı
+sns.countplot(data=veri,x="gogus_agrisi_tipi",hue="kalp_rahatsizligi")
+plt.show()
+
+# %%
+sns.catplot(data=veri,x="gogus_agrisi_tipi",hue="kalp_rahatsizligi",col="aclik_kan_sekeri",kind="count")
+
+plt.show()
